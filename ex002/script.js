4d50d81f455cea3a2c5c11d78b66f932d788cfdf
Lucas-Poeys <92745389+Lucas-Poeys@users.noreply.github.com>
2022-03-15 22:55:30 -0300

    aletraçãoes feitas na aula de 15/03/2022

---
+++ ex002/script.js
@@ -0,0 +1,19 @@
+function fechar(div) {
+   document.getElementById(div).style.display = "none"
+}
+
+function logar() {
+   let login = document.getElementById("login").value 
+   let senha = document.getElementById("senha").value
+   if (login == "admin" && senha == "admin") {
+      fechar()
+      alert("Bem vindo novamente Admin!")
+   }
+   else {
+      document.getElementById("msg").style.display = "block"
+   }
+}
+function aument(idv) {
+   document.getElementById("d").style.display = "block"
+   document.getElementById("imagem_grande").src = idv
+}

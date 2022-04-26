dc30dbcc85712323cbf5f5ccaf62281537e62563
Lucas Poeys Carpenter de Sant'Anna <92745389+Lucas-Poeys@users.noreply.github.com>
2022-04-26 14:24:43 -0300

    atualizei o ex005

--- ex005/js/script.js
+++ ex005/js/script.js
@@ -42,3 +42,37 @@ function opcionais(valor,marcado) {
     //saida
     document.getElementById('total').value = total;
 }
+function trocaTema() {
+    //entrada
+    tema = document.getElementById("tema").value;
+    //processamento
+    if (tema == "1") {
+        titulo = "Super Mario Brós";
+        newFundo = "img/mario.jpg";
+        newFoto = "img/mario.png";
+    }
+    else if (tema == "2") {
+        titulo = "Princesa Sofia";
+        newFundo = "img/sofia.jpg";
+        newFoto = "img/sofia.png";
+    }
+    else if (tema == "3") {
+        titulo = "Pj Masks"
+        newFundo = "img/pjmasks.jpg"
+        newFoto = "img/pjmasks.png"
+    }
+    else if (tema == "4") {
+        titulo = "Vasco"
+        newFundo = "img/vasco.jpg"
+        newFoto = "img/nene.png"
+    }
+    else {
+        titulo = "Tema da Festa"
+        newFundo = "    "
+        newFoto = ""
+    }
+    //saida
+    document.getElementById("titulo").value = titulo
+    document.body.style.backgroundImage = "url(" + newFundo + ")";
+    document.getElementById("foto").src = newFoto;
+}
